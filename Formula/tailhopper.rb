9d460550e9ffa001cf9478315a9f36abff880355
Joel Ambass <Jcambass@users.noreply.github.com>
2026-03-26 16:02:28 +0100

    tailhopper 0.1.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tailhopper.rb
+++ Formula/tailhopper.rb
@@ -1,8 +1,8 @@
 class Tailhopper < Formula
   desc "Use multiple Tailscale tailnets at the same time"
   homepage "https://github.com/jcambass/tailhopper"
-  url "https://github.com/Jcambass/tailhopper/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "810b039d030ef5d2855fa8a56e8f85e64909b35bb96836ab186fcd4acbbdaa3d"
+  url "https://github.com/Jcambass/tailhopper/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "294c926363a1a868cf76b648e1b67a03a981b6adc304191b885a0e230f762536"
   license "MIT"
   head "https://github.com/jcambass/tailhopper.git", branch: "main"
 
